563fc397314eaba5559e037fb514309593223cc2
Thomas Grubbe Sandborg Lauritsen <tgsl@cs.aau.dk>
2025-04-08 08:50:36 +0200

    A pump or fake pump will no longer be placed into the map if the cell it is to be placed in have not been seen yet.

--- ROS/map_processing.py
+++ ROS/map_processing.py
@@ -70,7 +70,8 @@ def process_map_data(drone_x: float, drone_y: float, map_config: MapConfig) -> S
         pump_x_index, pump_y_index = get_map_index_of_pump(state,pump)
         #print(pump_x_index, pump_y_index)
         if (pump_x_index >= state.map_width or pump_y_index >= state.map_height 
-            or pump_x_index < 0 or pump_y_index < 0):
+            or pump_x_index < 0 or pump_y_index < 0)\
+                or matrix[pump_y_index][pump_x_index] == -1:
             continue
         elif pump.has_been_discovered == False:
             matrix[pump_y_index][pump_x_index] = 2
